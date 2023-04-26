d5d28ef1b04669801a6072a9076abc52eede10e0
Asravin <maisweedfell@.com>
2023-04-26 16:50:16 +0400

    добавлено окно почты

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function get_answer(){
+    var input = document.getElementById("my_email").value;
+    if(document.getElementById("my_email").value === ""){
+        alert("Введите корректный адрес почты");
+    }else{
+        alert("Данные отправлены");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
